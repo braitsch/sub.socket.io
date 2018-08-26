0dce7b80c6bb081e44fe38e44c14a48e671960cc
braitsch <stephen@braitsch.io>
2018-08-26 16:43:51 -0700

    disabling 404 * to see if it is interfering with certbot

--- mydomain.com/app/server/routes.js
+++ mydomain.com/app/server/routes.js
@@ -6,8 +6,5 @@ module.exports = function(app) {
 	app.get('/', function(req, res){
 		res.render('index', { title: 'Node.js Chat Server'});
 	});	
-	app.get('*', function(req, res){
-		res.render('404', { title: 'Page Not Found'});
-	});
 	
 };
